77c899e3c7643c4b772742968868152acb6361d3
yeseul0809 <dresulsul@naver.com>
2024-07-21 17:56:35 +0900

    feat: quill 에디터 삽입

---
+++ src/app/(providers)/(root)/seller/mypage/[id]/products/register/_components/QuillEditor.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+import React, { useRef } from 'react';
+import ReactQuill, { ReactQuillProps } from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+const FORMATS = [
+  'size',
+  'bold',
+  'underline',
+  'blockquote',
+  'list',
+  'link',
+  'image',
+  'align',
+  'color',
+  'background'
+];
+
+interface ForwardedQuillComponent extends ReactQuillProps {
+  forwardedRef: React.Ref<ReactQuill>;
+}
+
+const DynamicReactQuill = dynamic(
+  async () => {
+    const { default: ReactQuill } = await import('react-quill');
+    const ForwardedComponent = ({ forwardedRef, ...props }: ForwardedQuillComponent) => (
+      <ReactQuill ref={forwardedRef} {...props} />
+    );
+    ForwardedComponent.displayName = 'ForwardedComponent';
+    return ForwardedComponent;
+  },
+  { ssr: false, loading: () => <p>Loading ...</p> }
+);
+
+function QuillEditor() {
+  const quillRef = useRef<ReactQuill | null>(null);
+
+  const modules = React.useMemo(
+    () => ({
+      toolbar: {
+        container: [
+          [{ size: ['small', false, 'large', 'huge'] }],
+          ['bold', 'underline', 'blockquote', 'link', 'image'],
+          [{ list: 'ordered' }, { list: 'bullet' }],
+          [{ align: [] }, { color: [] }, { background: [] }]
+        ]
+        // handlers: {
+        //   image: () => handleImage(quillRef)
+        // }
+      }
+    }),
+    []
+  );
+  return (
+    <>
+      <DynamicReactQuill
+        forwardedRef={quillRef}
+        theme="snow"
+        modules={modules}
+        formats={FORMATS}
+        placeholder="등록할 상품의 상세내용을 작성해주세요."
+        className="h-96 border-[#f0f0f0]"
+      />
+    </>
+  );
+}
+
+export default QuillEditor;
--- src/app/(providers)/(root)/seller/mypage/[id]/products/register/page.tsx
+++ src/app/(providers)/(root)/seller/mypage/[id]/products/register/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import QuillEditor from './_components/QuillEditor';
 
 function ProductRegisterPage() {
-  return <div>상품 등록 페이지</div>;
+  return (
+    <>
+      <h1>상품 등록 페이지</h1>
+      <QuillEditor />
+    </>
+  );
 }
 
 export default ProductRegisterPage;
