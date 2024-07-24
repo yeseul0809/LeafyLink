6869fa12ec556891aca4d50c90ce505775b8b148
teoyo <bigbang723@naver.com>
2024-07-25 03:40:32 +0900

    feat: footer

--- src/app/(providers)/(root)/_components/Footer.tsx
+++ src/app/(providers)/(root)/_components/Footer.tsx
@@ -1,8 +1,37 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 function Footer() {
   return (
-    <section>
+    <section className="w-full h-auto flex">
+      <div className="w-full h-auto px-[190px] py-[37px] bg-[#A6A6A6] text-[14px] text-[#505050]">
+        <Link href={'/'}>
+          <Image src="/icons/logo.svg" alt="logo" width={100} height={30}></Image>
+        </Link>
+        <div className="flex justify-left items-center mt-5 mb-7">
+          <a href="#">회사소개</a>
+          <span className="px-7">|</span>
+          <a href="#">이용약관</a>
+          <span className="px-7">|</span>
+          <a href="#" className="font-semibold text-black">
+            개인정보처리방침
+          </a>
+          <span className="px-7">|</span>
+          <a href="#">제휴광고</a>
+        </div>
+        <div className="leading-loose text-[13px]">
+          <p>
+            대표이사 : 십시일반 | 서울특별시 내배캠 유튜브 99, 스파르타 사옥
+            <br />
+            사업자등록번호 : 123-45-67890 | 통신판매업신고 : 9999-12345호
+            <br />
+            개인정보보호책임자 : 십시일반 | 이메일 : sipsiilban@naver.com
+            <br />
+            Copyright ⓒ SIPSIILBAN DESIGNER ALL RIGHTS RESERVED.
+          </p>
+        </div>
+      </div>
       <div></div>
     </section>
   );
--- src/app/(providers)/layout.tsx
+++ src/app/(providers)/layout.tsx
@@ -1,12 +1,14 @@
 import QueryProvider from '@/providers/QueryProvider';
 import { PropsWithChildren } from 'react';
 import Header from './(root)/_components/Header';
+import Footer from './(root)/_components/Footer';
 
 const ProvidersLayout = ({ children }: PropsWithChildren) => {
   return (
     <QueryProvider>
       <Header />
       {children}
+      <Footer />
     </QueryProvider>
   );
 };
