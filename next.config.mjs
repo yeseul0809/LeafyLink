c3f4e256a8e28d8236076d990f26b8c8572952ba
teoyo <bigbang723@naver.com>
2024-07-25 20:15:10 +0900

    fix: next.config에 hostname추가해서 구글 이미지 뜨게 고침

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
     remotePatterns: [
       {
         hostname: 'cirbxzxyrghkthxdsrpe.supabase.co'
+      },
+      {
+        hostname: 'lh3.googleusercontent.com'
       }
     ],
     domains: ['k.kakaocdn.net'] // 여기에 이미지 호스트명을 추가합니다.
