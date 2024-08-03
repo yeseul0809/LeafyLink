ea182d50b3a5035080d1fc26b6d9cd845153cd08
yeseul0809 <dresulsul@naver.com>
2024-08-03 14:47:57 +0900

    fix: 로그아웃 상태 유저정보 처리 수정

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -9,11 +9,8 @@ function useUser() {
   useEffect(() => {
     const fetchUser = async () => {
       const { data: userData, error } = await supabase.auth.getUser();
-      if (error) {
-        console.error('로그인한 유저 정보 가져오는 중 에러 발생', error);
-      } else {
-        setUser(userData?.user);
-      }
+
+      setUser(userData?.user);
     };
     fetchUser();
   }, [supabase.auth]);
