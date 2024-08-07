92cf7dbeb4012a9d37081846f3f1e7246e40bbdc
teoyo <bigbang723@naver.com>
2024-08-07 15:00:29 +0900

    fix: 빌드 오류 수정

--- src/app/(providers)/layout.tsx
+++ src/app/(providers)/layout.tsx
@@ -1,12 +1,8 @@
 import QueryProvider from '@/providers/QueryProvider';
 import { PropsWithChildren } from 'react';
-import Header from './(root)/_components/(header)/Header';
 import Footer from './(root)/_components/Footer';
 import TopButton from './(root)/_components/TopButton';
-import Carousel from './(root)/(home)/Carousel';
-import LiveCommerce from './(root)/(home)/LiveCommerce';
-import Advertising from './(root)/(home)/Advertising';
-import { usePathname } from 'next/navigation';
+import Header from './(root)/_components/(header)/Header';
 
 const ProvidersLayout = ({ children }: PropsWithChildren) => {
   return (
