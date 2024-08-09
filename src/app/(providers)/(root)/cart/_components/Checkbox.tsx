c4e72bf37163032d856636ecda550ad972132d4d
raccoonboy <raccoonboy0803@gmail.com>
2024-08-09 14:41:16 +0900

    refactor: 장바구니페이지 Checkbox컴포넌트 유즈쿼리 삭제&주스탠드 적용

--- src/app/(providers)/(root)/cart/_components/Checkbox.tsx
+++ src/app/(providers)/(root)/cart/_components/Checkbox.tsx
@@ -1,38 +1,27 @@
 'use client';
 
 import React from 'react';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
-import { getCartIsChecked, toggleCheckbox } from '../actions';
 import { useCartStore } from '@/stores';
 
 export default function Checkbox({ productId, userId }: { productId: string; userId: string }) {
-  const queryClient = useQueryClient();
-  const updateCartCheck = useCartStore((state) => state.updateItem);
+  const { cart, updateItem } = useCartStore((state) => ({
+    cart: state.cart,
+    updateItem: state.updateItem
+  }));
 
-  const {
-    data: isChecked,
-    error,
-    isFetched
-  } = useQuery({
-    queryKey: ['getCartIschecked', productId, userId],
-    queryFn: () => getCartIsChecked(productId, userId)
-  });
+  const isChecked = cart[productId]?.isChecked || false;
 
   const handleToggle = async () => {
-    const newCheckedStatus = !isChecked?.is_checked;
-    updateCartCheck(productId, newCheckedStatus, userId);
-
-    queryClient.invalidateQueries({ queryKey: ['getCartIschecked', productId, userId] });
+    const newCheckedStatus = !isChecked;
+    updateItem(productId, newCheckedStatus, userId);
   };
 
-  if (isFetched && isChecked) {
-    return (
-      <input
-        type="checkbox"
-        className="absolute -top-[0%] left-[0%] w-[18px] h-[18px] green-checkbox"
-        checked={isChecked?.is_checked}
-        onChange={handleToggle}
-      />
-    );
-  }
+  return (
+    <input
+      type="checkbox"
+      className="absolute -top-[0%] left-[0%] w-[18px] h-[18px] green-checkbox"
+      checked={isChecked}
+      onChange={handleToggle}
+    />
+  );
 }
