2ea02638392f2ae4625aad5671eaf00da1ef3b0a
teoyo <bigbang723@naver.com>
2024-08-14 21:12:06 +0900

    fix: 병합

--- src/app/(providers)/(root)/cart/_components/AllCheckbox.tsx
+++ src/app/(providers)/(root)/cart/_components/AllCheckbox.tsx
@@ -19,38 +19,9 @@ export default function AllCheckbox({
   }));
   const queryClient = useQueryClient();
 
-<<<<<<< HEAD
-  const { data: cartStatus, isFetched: isCartFetched } = useQuery({
-    queryKey: ['getCartStatus', userId],
-    queryFn: () => getCartStatus(userId!),
-    enabled: !!userId
-  });
-
-  useEffect(() => {
-    if (isCartFetched && cartStatus) {
-      const allChecked = cartStatus.every((item: any) => item.is_checked);
-      setIsChecked(allChecked);
-    }
-  }, [cartStatus, isCartFetched]);
-
-  useEffect(() => {
-    updateCartCheck(userId, isChecked);
-  }, [userId, isChecked, updateCartCheck]);
-
-=======
->>>>>>> e06f7b391bb0fa01d41271606c44b2a32ebac892
   const handleAllToggle = async () => {
     const newCheckedStatus = !selectAll;
     if (userId) {
-<<<<<<< HEAD
-      await allToggleCheckbox(userId, newCheckedStatus);
-
-      await Promise.all(
-        productIds.map((productId) =>
-          queryClient.invalidateQueries({ queryKey: ['getCartIschecked', productId] })
-        )
-      );
-=======
       toggleSelectAll(userId, productIds, newCheckedStatus);
 
       productIds.forEach((productId) => {
@@ -60,7 +31,6 @@ export default function AllCheckbox({
       });
 
       queryClient.invalidateQueries({ queryKey: ['getCartStatus', userId] });
->>>>>>> e06f7b391bb0fa01d41271606c44b2a32ebac892
     }
   };
 
