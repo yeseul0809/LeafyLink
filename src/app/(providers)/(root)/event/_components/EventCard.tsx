0527076ce43acdb305e5f9719b9f972c76aaa2c2
teoyo <bigbang723@naver.com>
2024-09-17 00:09:29 +0900

    feat: 이벤트 카드 생성

---
+++ src/app/(providers)/(root)/event/_components/EventCard.tsx
@@ -0,0 +1,31 @@
+'use client';
+import React, { useState } from 'react';
+
+function EventCard() {
+  const [eventType, setEventType] = useState('gift');
+  return (
+    <div className="w-[400px] text-center">
+      <div className="w-full h-[300px] border-[1px] relative p-4 rounded-[20px] mb-5">
+        {eventType === 'gift' ? (
+          <div className="w-[35px] h-[26px] absolute text-center text-[11px] text-white rounded-full bg-primary-green-500 flex items-center justify-center">
+            증정
+          </div>
+        ) : (
+          <div className="w-[35px] h-[26px] absolute text-center text-[11px] rounded-full bg-secondary-yellow-100 flex items-center justify-center">
+            할인
+          </div>
+        )}
+      </div>
+
+      <p className="w-full text-[20px] font-semibold truncate mb-1">이벤트 타이틀</p>
+      <p className="truncate text-font/sub1 text-[15px] mb-3">
+        이벤트 설명 설명 설며어어어어어어어엉이야이야이야ㅣ야이야ㅣ야이ㅑ이이야이ㅑㅇ
+      </p>
+      <p className="w-full">
+        <span className="pr-2 font-semibold text-primary-green-500">D-</span>24.09.16~24.09.16
+      </p>
+    </div>
+  );
+}
+
+export default EventCard;
--- src/app/(providers)/(root)/event/page.tsx
+++ src/app/(providers)/(root)/event/page.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import EventCard from './_components/EventCard';
 
 function Event() {
-  return <section>이벤트 페이지 입니다</section>;
+  return (
+    <section className="h-screen mx-auto lg:mt-[80px] lg:mb-[180px]">
+      <h2 className="text-[32px] text-center lg:mb-[48px] max_md:text-[20px] max_md:mt-6 max_md:mb-4 font-semibold">
+        이벤트
+      </h2>
+      <EventCard />
+    </section>
+  );
 }
 
 export default Event;
