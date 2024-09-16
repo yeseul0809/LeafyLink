e3e2bd86cf328d30b7d4719893092d226561c4d1
teoyo <bigbang723@naver.com>
2024-09-17 00:11:01 +0900

    feat: 이벤트 카드에 커서 포인터 추가

--- src/app/(providers)/(root)/event/_components/EventCard.tsx
+++ src/app/(providers)/(root)/event/_components/EventCard.tsx
@@ -4,8 +4,8 @@ import React, { useState } from 'react';
 function EventCard() {
   const [eventType, setEventType] = useState('gift');
   return (
-    <div className="w-[400px] text-center">
-      <div className="w-full h-[300px] border-[1px] relative p-4 rounded-[20px] mb-5">
+    <div className="w-[400px] text-center ">
+      <div className="w-full h-[300px] border-[1px] relative p-4 rounded-[20px] mb-5 cursor-pointer">
         {eventType === 'gift' ? (
           <div className="w-[35px] h-[26px] absolute text-center text-[11px] text-white rounded-full bg-primary-green-500 flex items-center justify-center">
             증정
@@ -17,8 +17,8 @@ function EventCard() {
         )}
       </div>
 
-      <p className="w-full text-[20px] font-semibold truncate mb-1">이벤트 타이틀</p>
-      <p className="truncate text-font/sub1 text-[15px] mb-3">
+      <p className="w-full text-[20px] font-semibold truncate mb-1 cursor-pointer">이벤트 타이틀</p>
+      <p className="truncate text-font/sub1 text-[15px] mb-3 cursor-pointer">
         이벤트 설명 설명 설며어어어어어어어엉이야이야이야ㅣ야이야ㅣ야이ㅑ이이야이ㅑㅇ
       </p>
       <p className="w-full">
