a16319c3394337ff9ed532c1bd4164fa7fd7992b
teoyo <bigbang723@naver.com>
2024-09-29 16:48:17 +0900

    design: 만료된 이벤트 카드 모바일 버전

--- src/app/(providers)/(root)/event/_components/EventCardExpired.tsx.tsx
+++ src/app/(providers)/(root)/event/_components/EventCardExpired.tsx.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 function EventCardExpired({ expiredEvent }: { expiredEvent: Event }) {
   return (
-    <div className="w-[400px] text-center">
+    <div className="w-[400px] text-center max_xs:w-[335px] max_xs:mx-auto">
       <div
-        className="w-full h-[300px] border-[1px] relative rounded-[20px] mb-5 bg-center bg-cover"
+        className="w-full h-[300px] max_xs:h-[251px] border-[1px] relative rounded-[20px] mb-5 bg-center bg-cover"
         style={{ backgroundImage: `url(${expiredEvent.thumbnail_url})` }}
       >
         <div className="w-full h-full bg-black opacity-60 rounded-[20px]">
@@ -15,9 +15,13 @@ function EventCardExpired({ expiredEvent }: { expiredEvent: Event }) {
         </div>
       </div>
 
-      <p className="w-full text-[20px] font-semibold truncate mb-1 ">{expiredEvent.title}</p>
-      <p className="truncate text-font/sub1 text-[15px] mb-3 ">{expiredEvent.summary}</p>
-      <p className="w-full">
+      <p className="w-full text-[20px] max_xs:text-[18px] font-semibold truncate mb-1 ">
+        {expiredEvent.title}
+      </p>
+      <p className="truncate text-font/sub1 text-[15px] max_xs:text-[14px] mb-3 ">
+        {expiredEvent.summary}
+      </p>
+      <p className="w-full max_xs:text-[14px]">
         <span className="pr-2 font-semibold text-font/Disabled">종료</span>
         {new Date(expiredEvent.event_starttime).toLocaleDateString()}~
         {new Date(expiredEvent.event_endtime).toLocaleDateString()}
